4564633e5b34bbc06877b93bf5e3ccb11cc71616
Unni <unni.tallman@gmail.com>
2014-06-17 21:33:19 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,25 @@
+var app = require('http').createServer(handler);
+
+app.listen(8000);
+
+var io    = require('socket.io').listen(app),
+    fs    = require('fs'),
+    sys   = require('sys'),
+    stdin = process.openStdin();
+
+
+function handler(req, res) {
+  fs.readFile(__dirname + '/index.html', function(err, data) {
+    res.writeHead(200);
+    res.end(data);
+  });
+}
+
+io.sockets.on('connection', function(socket) {
+  var json  = {};
+
+  stdin.addListener("data", function(d) {
+    json.name = d.toString();
+    socket.volatile.emit('notification', json);
+  });
+});
